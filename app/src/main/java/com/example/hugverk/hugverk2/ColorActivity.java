992717d535d41a9ac600ad2aa5099e52fd3bb4c7
gunnsteinnskula <gunnsteinnskula@gmail.com>
2017-03-08 02:51:16 +0000

    Refactor

---
+++ app/src/main/java/com/example/hugverk/hugverk2/ColorActivity.java
@@ -0,0 +1,16 @@
+package com.example.hugverk.hugverk2;
+
+import android.os.Bundle;
+
+/**
+ * Created by Gunnsteinn on 08/03/17.
+ */
+
+public class ColorActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_activity);
+    }
+}
