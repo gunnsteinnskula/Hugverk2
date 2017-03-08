809c1d1181e7c93d3c048bbd9679722467cb06de
gunnsteinnskula <gunnsteinnskula@gmail.com>
2017-03-08 01:00:49 +0000

    Classes

---
+++ app/src/main/java/com/example/hugverk/hugverk2/APIController.java
@@ -0,0 +1,8 @@
+package com.example.hugverk.hugverk2;
+
+/**
+ * Created by Gunnsteinn on 08/03/17.
+ */
+
+public class APIController {
+}
---
+++ app/src/main/res/APIController.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Gunnsteinn on 08/03/17.
+ */
+
+public class APIController {
+}
