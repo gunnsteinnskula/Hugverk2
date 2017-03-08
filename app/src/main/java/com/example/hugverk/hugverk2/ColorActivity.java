9abfb2bd6e5f82e61fdbc7217bf4d338d65c3429
gunnsteinnskula <gunnsteinnskula@gmail.com>
2017-03-08 05:51:41 +0000

    rgb hex colorname buttons

--- app/src/main/java/com/example/hugverk/hugverk2/ColorActivity.java
+++ app/src/main/java/com/example/hugverk/hugverk2/ColorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.R.attr.button;
@@ -22,18 +23,36 @@ public class ColorActivity extends AppCompatActivity {
 
         ColorDatabaseController.initdb();
 
-        Button colorButton = (Button) findViewById(R.id.color_button);
-        ColorDrawable buttonColor = (ColorDrawable) colorButton.getBackground();
-        final AppColor appcolor = new AppColor(buttonColor.toString());
+        TextView colorTextView = (TextView) findViewById(R.id.color_textview);
+        ColorDrawable textViewColor = (ColorDrawable) colorTextView.getBackground();
+        final AppColor appcolor = new AppColor(textViewColor.toString());
 
-        colorButton.setOnClickListener(new View.OnClickListener(){
+        //rgb_button, hex_button, colorname_button
+        Button rgbButton = (Button) findViewById(R.id.rgb_button);
+
+        rgbButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(ColorActivity.this, appcolor.getRgb() , Toast.LENGTH_SHORT).show();
-
-
+                Toast.makeText(ColorActivity.this, appcolor.getRgb() , Toast.LENGTH_LONG).show();
             }
         });
 
+        Button hexButton = (Button) findViewById(R.id.hex_button);
+
+        hexButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(ColorActivity.this, appcolor.getHex() , Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button colorNameButton = (Button) findViewById(R.id.colorname_button);
+
+        colorNameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(ColorActivity.this, appcolor.getColorName() , Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
